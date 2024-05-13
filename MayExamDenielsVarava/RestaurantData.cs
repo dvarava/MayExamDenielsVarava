7aaee778df6e03f5d82ccb470b83deb90454660c
Deniels Varava <S00240478@atu.ie>
2024-05-13 13:14:20 +0100

    created RestaurantData class that implements DBContext

---
+++ MayExamDenielsVarava/RestaurantData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MayExamDenielsVarava
+{
+    public class RestaurantData : DbContext
+    {
+        public RestaurantData() : base("OODExam_DenielsVarava") { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
