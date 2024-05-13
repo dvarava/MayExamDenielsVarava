cf89a034aca900c6d366477a60114a22fb33aa84
Deniels Varava <S00240478@atu.ie>
2024-05-13 13:05:49 +0100

    created Booking class

---
+++ MayExamDenielsVarava/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayExamDenielsVarava
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+    }
+}
